ae158a5fa8dcc83b2b73dbdd78750b2fc0455762
pavlushatkachev-max <pavlushatkachev@gmail.com>
2026-05-30 16:34:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #include "bst.h"
-
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 int main() {
-    return 0;
+  BST<std::string> tree;
+  makeTree(tree, "src/war_peace.txt");
+  printFreq(tree);
+  std::cout << "Высота дерева: " << tree.depth() << std::endl;
+  return 0;
 }
